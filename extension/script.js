d31da47a81422eb83b1d587e9774920f30c4df54
Dumbledore-web <james.a.newell@outlook.com>
2021-10-28 17:00:21 +1100

    Fix popup and remove stupid stuff

--- extension/script.js
+++
@@ -1,6 +0,0 @@
-<script src='https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' async defer>
-    new Crate({
-        server: '826023027548225547', // CGS
-        channel: '849767524967251968' // #cgs-general｜💬
-    })
-</script>
